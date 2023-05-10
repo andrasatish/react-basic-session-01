7451cd978391d304337484cee9be5b69c0cc4bae
sudheer <satishbabua02@gmail.com>
2023-05-10 14:01:49 +0530

    styles added in app css file

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
 import { Component } from "react";
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      users: []
+      users: [],
     };
   }
 
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then((users)=>{
-        this.setState({users})
-      })
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState({ users });
+      });
   }
 
   render() {
     const { users } = this.state;
     return (
-      <div className="users-container">
-        {
-          users.map((user)=>(
+      <div>
+        <div className="search-container">
+          <input className="search-input-field" placeholder="username" />
+        </div>
+        <div className="users-container">
+          {users.map((user) => (
             <div key={user.id} className="user-container">
               <h2>{user.name}</h2>
               <p>{user.email}</p>
               <p>{user.address.city}</p>
             </div>
-          ))
-        }
+          ))}
+        </div>
       </div>
     );
   }
