a0bac4fc117815505d44796fdf5ff636f1c6035e
sudheer <satishbabua02@gmail.com>
2023-05-10 12:50:41 +0530

    calling api on initial load

--- src/App.js
+++ src/App.js
@@ -5,87 +5,25 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: "Angular",
-      age: 50,
-      tvs: [
-        {
-          brand:
-            "iFFALCON by TCL U62 139 cm (55 inch) Ultra HD (4K) LED Smart...",
-          price: "32,999",
-          id: "12BDOPA23",
-        },
-        {
-          brand:
-            "SAMSUNG Series 8 189 cm (75 inch) Ultra HD (4K) LED Smart Tizen T...",
-          price: "59,990",
-          id: "12BDOPA24",
-        },
-        {
-          brand:
-            "Thomson 164 cm (65 inch) QLED Ultra HD (4K) Smart Google TV With ...",
-          price: "30,000",
-          id: "12BDOPA25",
-        },
-        {
-          brand:
-            "OnePlus U1S 164 cm (65 inch) Ultra HD (4K) LED Smart Android TV w...",
-          price: "90,000",
-          id: "12BDOPA26",
-        },
-        {
-          brand:
-            "acer I Series 139 cm (55 inch) Ultra HD (4K) LED Smart Android TV...",
-          price: "20,000",
-          id: "12BDOPA27",
-        },
-        {
-          brand:
-            "dell I Series 139 cm (55 inch) Ultra HD (4K) LED Smart Android TV...",
-          price: "20,000",
-          id: "12BDOPA29",
-        },
-      ],
+      users: []
     };
-    console.log('constructor');
   }
 
   componentDidMount(){
-    console.log('component did mount'); 
-  }
-
-  componentDidUpdate(){
-    console.log('component did update');
-  }
-
-  componentWillUnmount(){
-    console.log('component will unmount');
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then((users)=>{
+        this.setState({users})
+      })
   }
 
   render() {
-    console.log('render');
-    const { name, age, tvs } = this.state;
-    const changeNameHandler = () => {
-      this.setState({ name: "React" });
-    };
+    
     return (
       <div>
-        <h1>{name}</h1>
-        <p>{age}</p> &nbsp;
-        <button type="button" onClick={changeNameHandler}>
-          Change Name
-        </button>
-        <hr />
-        <div className="cards-user">
-          <h1>TV Brands</h1>
-          {tvs.map((tv, index) => (
-              <div key={tv.id}>
-                <p>{tv.brand}</p>
-                <p>{tv.price}</p>
-              </div>
-            )
-          )}
-          <p>Newly Added</p>
-        </div>
+        {
+         console.log(this.state.users) 
+        }
       </div>
     );
   }
